00a0036a03f7664fb98a680311bf43a39ca69523
Austin Carpenter <acarp20@icloud.com>
2020-12-11 15:09:45 +1000

    Add logic to prompt user for missing information

--- app/Console/Commands/MakeCodeowners.php
+++ app/Console/Commands/MakeCodeowners.php
@@ -9,17 +9,41 @@ class MakeCodeowners extends Command
 {
     use WritesFiles;
 
-    protected $signature = 'make:codeowners {--t|team=reviewers} {--o|organisation=codinglabsau} {--u|user=}';
+    protected $signature = 'make:codeowners {--o|organisation=} {--t|team=} {--u|user=}';
     protected $description = 'Create a new CODEOWNERS file.';
 
     public function handle(): int
     {
         $file_contents = '* @';
 
-        if ($this->option('user')) {
-            $file_contents .= $this->option('user');
+        $prefersOrganisation = null;
+        $providedInput = null;
+
+        $options = $this->options();
+        $organisation = $this->option('organisation');
+        $team = $this->option('team');
+        $user = $this->option('user');
+
+        if (!(isset($organisation)) && !(isset($team)) && !(isset($user))) {
+            $providedInput = false;
+            $this->error('Command executed without sufficient options.');
+            $preference = $this->choice('Please select whether you would like to generate a CODEOWNERS file for an organisation and team or a single user', ['Organisation and Team', 'User'], 0);
+            $prefersOrganisation = $preference == 'Organisation and Team';
         } else {
-            $file_contents .= $this->option('organisation') . '/' . $this->option('team');
+            $providedInput = true;
+            $prefersOrganisation = (isset($organisation) || isset($team));
+        }
+
+        if ($prefersOrganisation) {
+            if ($providedInput && (!(isset($organisation)) || !(isset($team)))) {
+                $this->error('There is missing information in order to generate a CODEOWNERS file for your organisation and team.');
+            }
+            $organisation ??= $this->ask('Enter your organisation name');
+            $team ??= $this->ask('Enter your team name');
+            $file_contents .= $organisation . '/' . $team;
+        } else {
+            $user ??= $this->ask('Enter the name of the user');
+            $file_contents .= $user;
         }
 
         $this->prepareAndWriteFile('CODEOWNERS', $file_contents, 'root', '.github/', true);
